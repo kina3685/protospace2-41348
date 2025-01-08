da9f999b09f64d4452f34a0e937e2a1f5a374b46
kina3685 <naoto_0507@icloud.com>
2025-01-08 16:23:59 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,50 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  def show
+    @comment = Comment.new
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id]) 
+
+    if @prototype.update(prototype_params) 
+      redirect_to prototype_path(@prototype), notice: 'プロトタイプが更新されました！'
+    else 
+      render :edit, alert: 'プロトタイプの更新に失敗しました。'
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
+
